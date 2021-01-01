18c4c88b5a74a98ec387788fff3dfcd1e7a267ee
L666no6 <72982866+L666no6@users.noreply.github.com>
2021-01-01 10:50:52 +0800

    Add files via upload

---
+++ template c++.cpp
@@ -0,0 +1,27 @@
+//项目名:
+//编号:
+/*
+ Easist
+ Easy
+ NotHard
+ NotEsay
+ Hard
+ QuietHard
+ WhatFAK
+ Special
+*/
+//制作人:林暄博
+
+#include<bits/stdc++.h>
+#include<windows.h>
+
+#include "Debuging.h" 
+
+using namespace std;
+
+DEBUGING=0;
+
+int main()
+{
+	return 0;
+}
