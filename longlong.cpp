4217ab138817ea9a55cecc67a8584438a9806be9
L666no6 <72982866+L666no6@users.noreply.github.com>
2021-03-07 15:15:21 +0800

    Add files via upload

--- longlong.cpp
+++ longlong.cpp
@@ -1,112 +1,237 @@
 #include "longlong.h"
 
-using namespace std;
-//private
-    void longlong::clean() { while (len > 1 && !d[len - 1]) len--; }
-//public
-    longlong longlong::operator = (const char* num) {
-        memset(d, 0, sizeof(d)); len = strlen(num);
-        for (int i = 0; i < len; i++) d[i] = num[len - 1 - i] - '0';
-        clean();
-        return *this;
-    }
-    longlong longlong::operator = (int num) {
-        len = 0;
-        while (num != 0) {
-            this->d[len] = (num % 10);
-            len++;
-            num = num / 10;
-         }
-        return *this;
-    }
-
-    longlong longlong::operator + (const longlong& b) {
-        longlong c = *this; int i;
-        for (i = 0; i < b.len; i++) {
-            c.d[i] += b.d[i];
-            if (c.d[i] > 9) c.d[i] %= 10, c.d[i + 1]++;
-        }
-        while (c.d[i] > 9) c.d[i++] %= 10, c.d[i]++;
-        c.len = max(len, b.len);
-        if (c.d[i] && c.len <= i) c.len = i + 1;
-        return c;
-    }
-    longlong longlong::operator - (const longlong& b) {
-        longlong c = *this; int i;
-        for (i = 0; i < b.len; i++) {
-            c.d[i] -= b.d[i];
-            if (c.d[i] < 0) c.d[i] += 10, c.d[i + 1]--;
-        }
-        while (c.d[i] < 0) c.d[i++] += 10, c.d[i]--;
-        c.clean();
-        return c;
-    }
-    longlong longlong::operator * (const longlong& b)const {
-        int i, j; longlong c; c.len = len + b.len;
-        for (j = 0; j < b.len; j++) for (i = 0; i < len; i++)
-            c.d[i + j] += d[i] * b.d[j];
-        for (i = 0; i < c.len - 1; i++)
-            c.d[i + 1] += c.d[i] / 10, c.d[i] %= 10;
-        c.clean();
-        return c;
-    }
-    longlong longlong::operator / (const longlong& b) {
-        int i, j;
-        longlong c = *this, a = 0;
-        for (i = len - 1; i >= 0; i--)
-        {
-            a = a * 10 + d[i];
-            for (j = 0; j < 10; j++) if (a < b * (j + 1)) break;
-            c.d[i] = j;
-            a = a - b * j;
-        }
-        c.clean();
-        return c;
-    }
-    longlong longlong::operator % (const longlong& b) {
-        int i, j;
-        longlong a = 0;
-        for (i = len - 1; i >= 0; i--)
-        {
-            a = a * 10 + d[i];
-            for (j = 0; j < 10; j++) if (a < b * (j + 1)) break;
-            a = a - b * j;
-        }
-        return a;
-    }
-    longlong longlong::operator += (const longlong& b) {
-        *this = *this + b;
-        return *this;
-    }
-
-    bool longlong::operator <(const longlong& b) const {
-        if (len != b.len) return len < b.len;
-        for (int i = len - 1; i >= 0; i--)
-            if (d[i] != b.d[i]) return d[i] < b.d[i];
-        return false;
-    }
-    bool longlong::operator >(const longlong& b) const { return b < *this; }
-    bool longlong::operator<=(const longlong& b) const { return !(b < *this); }
-    bool longlong::operator>=(const longlong& b) const { return !(*this < b); }
-    bool longlong::operator!=(const longlong& b) const { return b < *this || *this < b; }
-    bool longlong::operator==(const longlong& b) const { return !(b < *this) && !(b > *this); }
-
-    string longlong::str() const {
-        char s[maxn] = {};
-        for (int i = 0; i < len; i++) s[len - 1 - i] = d[i] + '0';
-        return s;
-    }
-//std
-istream& operator >> (istream& in, longlong& x)
+longlong longlong::operator=(longlong l)
 {
-    string s;
-    in >> s;
-    x = s.c_str();
-    return in;
+	this->b = l.b;
+	this->sign = l.sign;
+	return longlong();
 }
 
-ostream& operator << (ostream& out, const longlong& x)
+longlong longlong::operator=(unsign b)
 {
-    out << x.str();
-    return out;
+	this->b = b;
+	this->sign = 0;
+	return longlong();
+}
+
+longlong longlong::operator=(int a)
+{
+	this->sign = 0;
+	if (a < 0) {
+		this->sign = 1;
+		a = -a;
+	}
+	this->b = a;
+	return longlong();
+}
+
+longlong longlong::operator+(longlong l)
+{
+	longlong a = *this;
+	if (this->sign == 0 and l.sign == 0) {
+		a.b = a.b + l.b;
+		a.sign = 0;
+	}
+	else if (this->sign == 1 and l.sign == 1) {
+		a.b = a.b + l.b;
+		a.sign = 1;
+	}
+	else {
+		if (a.b == l.b) {
+			a.b = 0;
+			a.sign = 0;
+		}
+		else if (a.b > l.b) {
+			a.b = a.b - l.b;
+		}
+		else {
+			a.b = l.b - a.b;
+		}
+		a.sign = !a.sign;
+	}
+	return a;
+}
+
+longlong longlong::operator-(longlong l)
+{
+	longlong a = *this;
+	if (a.sign == 0 and l.sign == 0) {
+		if (a.b >= l.b) {
+			a.b = a.b - l.b;
+			a.sign = 0;
+		}
+		else {
+			a.b = l.b - a.b;
+			a.sign = 1;
+		}
+	}
+	else if (a.sign == 1 and l.sign == 1) {
+		if (a.b >= l.b) {
+			a.b = a.b - l.b;
+			a.sign = 1;
+		}
+		else {
+			a.b = l.b - a.b;
+			a.sign = 0;
+		}
+	}
+	else {
+		if (a.sign = 0) {
+			a.b = a.b + l.b;
+			a.sign = 0;
+		}
+		else {
+			a.b = a.b + l.b;
+			a.sign = 1;
+		}
+	}
+	return a;
+}
+
+longlong longlong::operator*(longlong l)
+{
+	longlong r;
+	r.b = this->b * l.b;
+	if (this->sign == l.sign) {
+		r.sign = 0;
+	}
+	else {
+		r.sign = 1;
+	}
+	return r;
+}
+
+longlong longlong::operator/(longlong l)
+{
+	longlong r;
+	r.b = this->b / l.b;
+	if (this->sign == l.sign) {
+		r.sign = 0;
+	}
+	else {
+		r.sign = 1;
+	}
+	return r;
+	return longlong();
+}
+
+longlong longlong::operator%(longlong l)
+{
+	longlong a = *this;
+	if (a.sign == false and l.sign==false) {
+		a.b = a.b % l.b;
+	}
+	else if (a.sign == l.sign == true) {
+		a.b = a.b % l.b;
+		a.sign = 1;
+	}
+	else {
+		if (a.sign = 1) {
+			a.b = a.b % l.b;
+			a.b = l.b - a.b;
+			a.sign = 0;
+		}
+	}
+	return a;
+}
+
+longlong longlong::operator++()
+{
+	*this = *this + 1;
+	return *this;
+}
+
+longlong longlong::operator--()
+{
+	*this = *this - 1;
+	return *this;
+}
+
+longlong longlong::operator+=(longlong l)
+{
+	*this = *this + l;
+	return longlong();
+}
+
+longlong longlong::operator-=(longlong l)
+{
+	*this = *this - l;
+	return *this;
+}
+
+longlong longlong::operator*=(longlong l)
+{
+	*this = *this * l;
+	return *this;
+}
+
+bool longlong::operator<(longlong l)
+{
+	if (this->sign < l.sign)return false;
+	if (this->sign > l.sign)return true;
+	unsign x, y;
+	x = this->b;
+	y = l.b;
+	if (this->sign == l.sign == 0) return x < y;
+	if (this->sign == l.sign == 1)return x > y;
+	return false;
+}
+
+bool longlong::operator>(longlong l)
+{
+	return *this>=l and *this!=l;
+}
+
+bool longlong::operator==(longlong l)
+{
+	if (this->sign != l.sign) return false;
+	if (this->b != l.b)return false;
+	return true;
+}
+
+bool longlong::operator!=(longlong l)
+{
+	return !(*this==l);
+}
+
+bool longlong::operator>=(longlong l)
+{
+	return !(*this<l);
+}
+
+bool longlong::operator<=(longlong l)
+{
+	return !(*this>l);
+}
+
+void longlong::x16(std::string s)
+{
+	this->b.x16(s);
+}
+
+std::istream& operator>>(std::istream& in, longlong& l)
+{
+	std::string s;
+	in >> s;
+	l.sign = 0;
+	if (s[0] == '-') {
+		l.sign = 1;
+		std::string r;
+		r = s;
+		s = "";
+		for (int i = 1; i < r.length(); i++) {
+			s += r[i];
+		}
+	}
+	l.b = s.c_str();
+	return in;
+}
+
+std::ostream& operator<<(std::ostream& out, longlong& l)
+{
+	if (l.sign) {
+		out << '-';
+	}
+	out << l.b;
+	return out;
 }
--- longlong.h
+++ longlong.h
@@ -1,38 +1,52 @@
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <cstdio>
-using namespace std;
-
-const int maxn = 1000;
-
-class longlong {
+#pragma once
+#include "unsign.h"
+class longlong
+{
 private:
-    int d[maxn], len;
-
-    void clean();
+	unsign b;
+	bool sign;
 public:
-    longlong() { memset(d, 0, sizeof(d)); len = 1; }
-    longlong(int num) { *this = num; }
-    longlong(char* num) { *this = num; }
-    longlong operator = (const char* num);
-    longlong operator = (int num);
-    longlong operator + (const longlong& b);
-    longlong operator - (const longlong& b);
-    longlong operator * (const longlong& b)const;
-    longlong operator / (const longlong& b);
-    longlong operator % (const longlong& b);
-    longlong operator += (const longlong& b);
-    bool operator <(const longlong& b) const;
-    bool operator >(const longlong& b) const;
-    bool operator<=(const longlong& b) const;
-    bool operator>=(const longlong& b) const;
-    bool operator!=(const longlong& b) const;
-    bool operator==(const longlong& b) const;
+	longlong() {
+		b = 0;
+		sign = 0;
+	}
+	longlong(const longlong& obj) {
+		this->sign = obj.sign;
+		this->b = obj.b;
+	}
+	longlong(unsign a) {
+		this->b = a;
+		sign = 0;
+	}
+	longlong(int a) {
+		*this = a;
+	}
+	longlong operator=(longlong l);
+	longlong operator=(unsign b);
+	longlong operator=(int a);
 
-    string str() const;
+	longlong operator+(longlong l);
+	longlong operator-(longlong l);
+	longlong operator*(longlong l);
+	longlong operator/(longlong l);
+	longlong operator%(longlong l);
+
+	longlong operator++();
+	longlong operator--();
+	longlong operator+=(longlong l);
+	longlong operator-=(longlong l);
+	longlong operator*=(longlong l);
+	
+	bool operator<(longlong l);
+	bool operator>(longlong l);
+	bool operator==(longlong l);
+	bool operator!=(longlong l);
+	bool operator>=(longlong l);
+	bool operator<=(longlong l);
+
+	void x16(std::string s);
+
+	friend std::istream& operator >>(std::istream& in, longlong& l);
+	friend std::ostream& operator<<(std::ostream& out, longlong& l);
 };
 
-istream& operator >> (istream& in, longlong& x);
-
-ostream& operator << (ostream& out, const longlong& x);
